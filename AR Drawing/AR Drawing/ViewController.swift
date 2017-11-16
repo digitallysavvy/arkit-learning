a84a628a4adf9a834b49fead799e0d8b8a648e6e
digitallysavvy <hermes.frangoudis@gmail.com>
2017-11-15 23:44:07 -0500

    Project 2: get camera position

--- AR Drawing/AR Drawing/ViewController.swift
+++ AR Drawing/AR Drawing/ViewController.swift
@@ -30,8 +30,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
         print("rendering")
+        // get camera translation and rotation
+        guard let pointOfView = sceneView.pointOfView else { return }
+        let transform = pointOfView.transform // transformation matrix
+        let orientation = SCNVector3(transform.m31, transform.m32, transform.m33) // camera rotation
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43) // camera translation
+        
+        let currentPostion = orientation + location
+        print(orientation.x, orientation.y, orientation.z)
     }
 
 
 }
 
+func + (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
